811d0ca482e5c87e933cc0f98586bff4fd101f29
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-04-30 17:41:43 +0800

    Add files via upload

--- 27/sol.py
+++ 27/sol.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        jump = -1
+        for i in range(0, len(nums)):
+            if nums[i] != val:
+                jump = jump + 1;
+                nums[jump] = nums[i];
+        return jump + 1;
