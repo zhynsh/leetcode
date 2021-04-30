bd5709d71592280dbb744ec24774b80e005e89f3
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-04-30 17:37:44 +0800

    Add files via upload

--- 19/sol.py
+++ 19/sol.py
@@ -1,1 +1,18 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        copy = head;
+        while head != None:
+            temp = head;
+            for i in range(0, n):
+                temp = temp.next;
+            if temp == None:
+                return copy.next;
+            if temp.next == None:
+                head.next = head.next.next;
+                return copy;
+            head = head.next;
