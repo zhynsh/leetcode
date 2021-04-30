e8f5cc0e48639b079aef0b1e79d2964c3daa791b
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-04-30 17:40:37 +0800

    Add files via upload

--- 16/sol.py
+++ 16/sol.py
@@ -1,1 +1,34 @@
-
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort();
+        out = nums[0] + nums[1] + nums[2];
+        for i in range(0, len(nums) - 2):
+            if nums[i] + nums[i + 1] + nums[i + 2] > target:
+                if abs(out - target) > abs(nums[i] + nums[i + 1] + nums[i + 2] - target):
+                    out = nums[i] + nums[i + 1] + nums[i + 2];
+                break;
+            if nums[i] + nums[len(nums) - 1] + nums[len(nums) - 2] < target:
+                if abs(out - target) > abs(nums[i] + nums[len(nums) - 1] + nums[len(nums) - 2] - target):
+                    if i < len(nums) - 2:
+                        out = nums[i] + nums[len(nums) - 1] + nums[len(nums) - 2];
+                continue;
+            for j in range(i + 1, len(nums) - 1):
+                if nums[i] + nums[j] + nums[j + 1] > target:
+                    if abs(out - target) > abs(nums[i] + nums[j] + nums[j + 1] - target):
+                        out = nums[i] + nums[j] + nums[j + 1];
+                    break;
+                if nums[i] + nums[j] + nums[len(nums) - 1] < target:
+                    if abs(out - target) > abs(nums[i] + nums[j] + nums[len(nums) - 1] - target):
+                        if j < len(nums) - 1:
+                            out = nums[i] + nums[j] + nums[len(nums) - 1];
+                    continue;
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[i] + nums[j] + nums[k - 1] > target:
+                        break;
+                    if k < len(nums) - 1 and nums[i] + nums[j] + nums[k + 1] < target:
+                        continue;
+                    if nums[i] + nums[j] + nums[k] == target:
+                        return target;
+                    if abs(out - target) > abs(nums[i] + nums[j] + nums[k] - target):
+                        out = nums[i] + nums[j] + nums[k];
+        return out;
