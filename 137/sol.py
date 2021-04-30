4faf8c81bf12e1402adbd5555aa34a322f7b94fd
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-04-30 17:42:08 +0800

    Add files via upload

--- 137/sol.py
+++ 137/sol.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort();
+        if len(nums) < 2:
+            return nums[0];
+        i = 1;
+        while i < len(nums):
+            if nums[i] != nums[i - 1]:
+                return nums[i - 1];
+            i = i + 3;
+        return nums[len(nums) - 1];
