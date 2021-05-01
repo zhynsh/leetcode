060b6159ad505cb05828119bca5c2fc8b541dabf
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-01 13:57:11 +0800

    Add files via upload

--- 86/sol.py
+++ 86/sol.py
@@ -1,1 +1,37 @@
-
+#https://leetcode-cn.com/problems/partition-list/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        copy = head;
+        lbegin = None;
+        rbegin = None;
+        while head != None and (lbegin == None or rbegin == None):
+            if head.val < x:
+                if lbegin == None:
+                    lbegin = head;
+            else:
+                if rbegin == None:
+                    rbegin = head;
+            head = head.next;
+        if lbegin == None or rbegin == None:
+            return copy;
+        head = copy;
+        copy = lbegin;
+        temp = rbegin;
+        while head != None:
+            if head.val < x:
+                if head != lbegin:
+                    lbegin.next = head;
+                    lbegin = lbegin.next;
+            else:
+                if head != rbegin:
+                    rbegin.next = head;
+                    rbegin = rbegin.next;
+            head = head.next;
+        lbegin.next = temp;
+        rbegin.next = None;
+        return copy;
