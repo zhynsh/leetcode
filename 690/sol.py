523a98fd6330b7309459daed57e81106e0fc9ffe
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-01 12:23:37 +0800

    Add files via upload

--- 690/sol.py
+++ 690/sol.py
@@ -1,1 +1,18 @@
+#https://leetcode-cn.com/problems/employee-importance/
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
 
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        for i in range(0, len(employees)):
+            if employees[i].id == id:
+                imp = employees[i].importance;
+                for j in range(0, len(employees[i].subordinates)):
+                    imp = imp + self.getImportance(employees, employees[i].subordinates[j]);
+                return imp;
