62e5a28fb84a75a0613cc7b07a3823aedef2d7c9
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-04 03:12:19 +0800

    Add files via upload

--- 94/sol.py
+++ 94/sol.py
@@ -1,1 +1,20 @@
-
+#https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root == None:
+            return [];
+        def helper(root: TreeNode, sol: List[int]):
+            if root.left != None:
+                helper(root.left, sol);
+            sol.append(root.val);
+            if root.right != None:
+                helper(root.right, sol);
+        sol = [];
+        helper(root, sol);
+        return sol;
