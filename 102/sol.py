8acd9e6ea048a501f320c361a2c9eb52df93e86c
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-05 05:57:47 +0800

    Add files via upload

--- 102/sol.py
+++ 102/sol.py
@@ -1,1 +1,29 @@
-
+#https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root == None:
+            return [];
+        prevlev = [];
+        nextlev = [];
+        sol = [];
+        soltemp = [];
+        prevlev.append(root);
+        while len(prevlev) > 0:
+            while len(prevlev) > 0:
+                temp = prevlev.pop(0);
+                if temp.left != None:
+                    nextlev.append(temp.left);
+                if temp.right != None:
+                    nextlev.append(temp.right);
+                soltemp.append(temp.val);
+            sol.append(soltemp);
+            prevlev = nextlev;
+            nextlev = [];
+            soltemp = [];
+        return sol;
