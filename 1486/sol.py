c79137689dc0961d4ee2bb8f2fe772498df18dc8
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-07 01:02:38 +0800

    Add files via upload

--- 1486/sol.py
+++ 1486/sol.py
@@ -1,1 +1,7 @@
-
+#https://leetcode-cn.com/problems/xor-operation-in-an-array/
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        sol = start;
+        for i in range(1, n):
+            sol = sol ^ (start + 2 * i);
+        return sol;
