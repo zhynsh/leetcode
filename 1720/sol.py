5d2875f73da67346a8c5156769b38d6478dcb9f3
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-06 23:56:31 +0800

    Add files via upload

--- 1720/sol.py
+++ 1720/sol.py
@@ -1,1 +1,8 @@
-
+#https://leetcode-cn.com/problems/decode-xored-array/
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        sol = [];
+        sol.append(first);
+        for i in range(0, len(encoded)):
+            sol.append(encoded[i] ^ sol[-1]);
+        return sol;
