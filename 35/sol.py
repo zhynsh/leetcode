6019ed4df3063cc1429223aa17f376a6be42931d
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-09 20:40:48 +0800

    Add files via upload

--- 35/sol.py
+++ 35/sol.py
@@ -1,1 +1,7 @@
-
+#https://leetcode-cn.com/problems/search-insert-position/
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(0, len(nums)):
+            if nums[i] >= target:
+                return i;
+        return len(nums);
