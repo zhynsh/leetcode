2dc15e5c30ac852f7b01ecc71c6a217ee7bfd815
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-09 20:40:19 +0800

    Add files via upload

--- 33/sol.py
+++ 33/sol.py
@@ -1,7 +1,22 @@
-#https://leetcode-cn.com/problems/xor-operation-in-an-array/
+#https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
 class Solution:
-    def xorOperation(self, n: int, start: int) -> int:
-        sol = start;
-        for i in range(1, n):
-            sol = sol ^ (start + 2 * i);
-        return sol;
+    def search(self, nums: List[int], target: int) -> int:
+        if target < nums[0]:
+            for i in range(0, len(nums)):
+                if target == nums[len(nums) - 1 - i]:
+                    return len(nums) - 1 - i;
+                elif target > nums[len(nums) - 1 - i]:
+                    return -1;
+                elif nums[0] <= nums[len(nums) - 1 - i]:
+                    return -1;
+        elif target == nums[0]:
+            return 0;
+        else:
+            for i in range(1, len(nums)):
+                if target == nums[i]:
+                    return i;
+                elif target < nums[i]:
+                    return -1;
+                elif nums[0] >= nums[i]:
+                    return -1;
+        return -1;
