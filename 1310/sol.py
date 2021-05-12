8c31e1ba052dd52d005f0acb4b4c91f7242f5b55
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-12 23:59:12 +0800

    Add files via upload

--- 1310/sol.py
+++ 1310/sol.py
@@ -1,1 +1,12 @@
-
+#https://leetcode-cn.com/problems/xor-queries-of-a-subarray/
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        sol = [];
+        for i in range(0, len(queries)):
+            temp = arr[queries[i][0]];
+            index = queries[i][0];
+            for j in range(0, queries[i][1] - queries[i][0]):
+                index = index + 1;
+                temp = temp ^ arr[index];
+            sol.append(temp);
+        return sol;
