95199037b4e6f1002f1de64490764365249e95a9
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-12 23:59:44 +0800

    Add files via upload

--- 9/sol.py
+++ 9/sol.py
@@ -1,1 +1,12 @@
-
+#https://leetcode-cn.com/problems/palindrome-number/
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False;
+        if x == 0:
+            return True;
+        y = str(x);
+        for i in range(0, int(len(y) / 2)):
+            if y[i] != y[- i - 1]:
+                return False;
+        return True;
