2f502dff6293b4c5b19fd2a4b6d14abfe99e82be
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-05-19 13:50:43 +0800

    Update sol.py

--- 1738/sol.py
+++ 1738/sol.py
@@ -1,1 +1,19 @@
-
+#https://leetcode-cn.com/problems/find-kth-largest-xor-coordinate-value/
+class Solution:
+    def kthLargestValue(self, matrix: List[List[int]], k: int) -> int:
+        sol = [];
+        orders = [];
+        for i in range(len(matrix)):
+            sol.append([]);
+            for j in range(len(matrix[0])):
+                temp = matrix[i][j];
+                if i > 0:
+                    temp = temp ^ sol[i - 1][j];
+                if j > 0:
+                    temp = temp ^ sol[i][j - 1];
+                if i > 0 and j > 0:
+                    temp = temp ^ sol[i - 1][j - 1];
+                sol[i].append(temp);
+                orders.append(temp);
+        orders.sort();
+        return orders[-k];
