21e5d0b16791be1ac16df33eff2cfae377d85a15
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-06-16 12:01:29 +0800

    Update sol.py

--- 199/sol.py
+++ 199/sol.py
@@ -1,1 +1,25 @@
-
+#https://leetcode-cn.com/problems/binary-tree-right-side-view/
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if root == None:
+            return []
+        sol = []
+        def rsv(rlist: List[TreeNode]):
+            sol.append(rlist[-1].val)
+            temp = []
+            for node in rlist:
+                if node.left != None:
+                    temp.append(node.left)
+                if node.right != None:
+                    temp.append(node.right)
+            if len(temp) > 0:
+                rsv(temp)
+        t = []
+        t.append(root)
+        rsv(t)
+        return sol
