ddf0462d5bd26187e45cf50443f31241208456f2
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-06-17 17:53:05 +0800

    Update sol.py

--- 20/sol.py
+++ 20/sol.py
@@ -1,1 +1,21 @@
-
+#https://leetcode-cn.com/problems/valid-parentheses/
+class Solution:
+    def isValid(self, s: str) -> bool:
+        temp = []
+        for i in s:
+            if i == '(' or i == "[" or i == "{":
+                temp.append(i)
+            else:
+                if temp == []:
+                    return False
+                t = temp.pop()
+                if t == "(" and i == ")":
+                    continue
+                if t == "[" and i == "]":
+                    continue
+                if t == "{" and i == "}":
+                    continue
+                return False
+        if temp == []:
+            return True
+        return False
