698a432e4e024fe84a11782ac750574c839f2583
zhynsh <67862258+zhynsh@users.noreply.github.com>
2021-06-21 11:41:25 +0800

    Update sol.py

--- 401/sol.py
+++ 401/sol.py
@@ -1,1 +1,16 @@
-
+#https://leetcode-cn.com/problems/binary-watch/
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        times = []
+        def binary(a: int):
+            return len(bin(a).split('1')) - 1
+        for a in range(12):
+            if binary(a) <= turnedOn:
+                for b in range(60):
+                    if binary(b) == turnedOn - binary(a):
+                        timestr = str(a) + ':'
+                        if b < 10:
+                            timestr += '0'
+                        timestr += str(b)
+                        times.append(timestr)
+        return times
